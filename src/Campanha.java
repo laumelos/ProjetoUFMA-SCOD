a5ad91abad01f00e63e51531018805024c657836
laumelos <lauraemelos@gmail.com>
2022-10-22 11:38:43 -0300

    first commit

---
+++ src/Campanha.java
@@ -0,0 +1,45 @@
+public class Campanha {
+
+    protected String nome;
+    protected ArrayList<Itens> meta;
+    protected String tempo;
+
+    public Campanha(String nome, ArrayList<Itens> meta, String tempo) {
+        this.nome = nome;
+        this.meta = meta;
+        this.tempo = tempo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public ArrayList<Itens> getMeta() {
+        return meta;
+    }
+
+    public void setMeta(ArrayList<Itens> meta) {
+        this.meta = meta;
+    }
+
+    public String getTempo() {
+        return tempo;
+    }
+
+    public void setTempo(String tempo) {
+        this.tempo = tempo;
+    }
+
+    @Override
+    public String toString() {
+        return "Campanha{" +
+                "nome='" + nome + '\'' +
+                ", meta=" + meta +
+                ", tempo='" + tempo + '\'' +
+                '}';
+    }
+}
